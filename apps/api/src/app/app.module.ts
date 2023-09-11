01d1e6ae25300abe3b2bab64e89ccbca84541a99
xd88021as <s560798212@gmail.com>
2023-09-11 20:12:16 +0800

    feat(api): update app module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { configuration } from '@test/api/common/config';
 import { UserModule } from '@test/api/core/user';
+import { AuthModule } from '@test/api/feature/auth';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    AuthModule,
     ConfigModule.forRoot({
       load: [configuration],
       isGlobal: true,
