d5afed7f67f8a087b53d2f7a6d55942b3eb5b989
xd88021as <s560798212@gmail.com>
2023-09-11 16:06:08 +0800

    feat(api): update libs/api/core/user

--- libs/api/core/user/src/lib/services/user.service.ts
+++ libs/api/core/user/src/lib/services/user.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CryptoService } from '@test/api/common/crypto';
 import { ObjectService } from '@test/api/util/object';
-import { UserFindManyParams, UserFindUniqueParams } from '../interfaces/user.interface';
+import { UserData, UserFindManyParams, UserFindUniqueParams } from '../interfaces/user.interface';
 import { UserRepository } from '../repositories/user.repository';
 
 @Injectable()
 export class UserService {
   constructor(
+    private readonly cryptoService: CryptoService,
     private readonly objectService: ObjectService,
     private readonly userRepository: UserRepository
   ) {}
+
+  async create(data: UserData) {
+    data.password = await this.cryptoService.hash(data.password);
+    const user = await this.userRepository.create(data);
+    return user;
+  }
 
   async findMany(params: UserFindManyParams) {
     const users = await this.userRepository.findMany(params);
