c43427d4a0470da492b0cf7684038cbfb22d974b
xd88021as <s560798212@gmail.com>
2023-10-03 23:41:55 +0800

    feat(shared):  update libs/shared/data-access/commodity

---
+++ libs/shared/data-access/commodity/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/dto/commodity-param.dto';
+export * from './lib/dto/commodity-query.dto';
+export * from './lib/dto/commodity-response.dto';
+export * from './lib/dto/shop-commodity-body.dto';
+export * from './lib/dto/shop-commodity-param.dto';
